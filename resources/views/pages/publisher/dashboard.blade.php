f5630ec28c27b5499419527a315f1c2d48ffaaea
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-06-11 15:37:00 +0700

    Membuat card buku

--- resources/views/pages/publisher/dashboard.blade.php
+++ resources/views/pages/publisher/dashboard.blade.php
@@ -57,6 +57,52 @@
     <h4 class="d-inline">Data Buku</h4>
     <button class="button d-inline float-right" id="btnTambahBuku">Tambah Buku</button>
   </div>
+  <div class="container-fluid">
+    <div class="row card-book">
+      <div class="col-2">
+        <img 
+          src="{{ url('image/book_placeholder.png') }}" 
+          alt=""
+          class="ebook-image">
+      </div>
+      <div class="col-10">
+        <h4>Judul Buku</h4>
+        <p
+          class="font-weight-bold author-info"><span>Ditulis oleh </span><span class="author-text">Nama Penulis</span></p>
+        <p
+          class="synopsis">Seasonal, mocha grinder, body siphon filter cup dripper affogato flavour. Robusta frappuccino cup wings macchiato,  chicory latte, rich cream flavour 
+          extraction mazagran. Dark, medium crema dark kopi-luwak bar kopi-luwak. Aroma qui trifecta, cup, crema shop a affogato. Flavour, extra spoon,
+          est grounds redeye xtraction mazagran. Dark, medium crema dark kopi-luwak bar kopi-luwak. Aroma qui trifecta, cup, cr,
+          est grounds redeye xtraction mazagran. Dark, medium crema dark kopi-luwak bar kopi-luwak. Aroma qui trifecta, cup, cr</p>
+        <div class="book-rating-container">
+          <div class="star-container d-inline">
+            <img 
+              src="{{ url('image/icon/yellow_star.png') }}"
+              alt=""
+              class="star-image">
+            <img 
+              src="{{ url('image/icon/yellow_star.png') }}"
+              alt=""
+              class="star-image">
+            <img 
+              src="{{ url('image/icon/yellow_star.png') }}"
+              alt=""
+              class="star-image">
+            <img 
+              src="{{ url('image/icon/blank_star.png') }}"
+              alt=""
+              class="star-image">
+            <img
+              src="{{ url('image/icon/blank_star.png') }}"
+              alt=""
+              class="star-image">
+          </div>
+          <p class="d-inline mt-1">3.4 &emsp; (12 Ulasan) &emsp; 34x terjual</p>
+        </div>
+        <p class="price font-weight-bold">Rp. 34.000</p>
+      </div>
+    </div>
+  </div>
 @endsection
 
 @push('add-on-script')
