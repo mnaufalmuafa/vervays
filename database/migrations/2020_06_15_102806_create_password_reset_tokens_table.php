0c8649a9724e00807e027f7cd056b73cc0239a57
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-06-15 10:32:03 +0700

    Migration : create_password_reset_tokens_table

---
+++ database/migrations/2020_06_15_102806_create_password_reset_tokens_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasswordResetTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->integer('userId');
+            $table->string('token');
+            $table->timestamps();
+
+            $table->foreign('userId')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('password_reset_tokens', function (Blueprint $table) {
+            $table->dropForeign(['userId']);
+        });
+        Schema::dropIfExists('password_reset_tokens');
+    }
+}
