215de1bdb180891d6eae8595372d03ced81da262
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-06-22 11:27:16 +0700

    set bookCard onClickListener pada dashboard

--- public/js/view/buyer/dashboard.js
+++ public/js/view/buyer/dashboard.js
@@ -19,4 +19,31 @@ $(document).ready(function(){
       $('#'+id+' .fifth-star').attr("src","/image/icon/yellow_star.png");
     }
   });
+  $('.book-card').click(function() { //set bookCard onClickListener
+    var id = $(this).attr("id");
+    var title= $('#'+id+" p.bookTitle").html();
+    id = id.replace("book-card-", "");
+    id = id.replace("-newest", "");
+    id = id.replace("-bestseller", "");
+    id = id.replace("-editor-choice", "");
+    window.location.href = "/book/detail/"+id+"/"+string_to_slug(title);
+  });
 });
+
+function string_to_slug (str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to   = "aaaaeeeeiiiioooouuuunc------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+}
