466945accbc07334b866e2f4220271818c775b43
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-06-26 10:35:40 +0700

    setCardBookOnClickListener di hal dashboard publisher

--- public/js/view/publisher/dashboard.js
+++ public/js/view/publisher/dashboard.js
@@ -42,8 +42,36 @@ $(document).ready(function() {
       $('#'+id+' .fifth-star').attr("src","/image/icon/yellow_star.png");
     }
   });
+  setCardBookOnClickListener();
   $('.btn-edit-buku').click(function() {
     var id = $(this).attr("book-id");
     window.location.href = "/publisher/edit/book?id="+id;
   });
 });
+
+function setCardBookOnClickListener() {
+  $('.card-book').click(function() {
+    var id = $(this).attr("id");
+    var title = $("#"+id+' .book-title').html();
+    id = id.replace("book-card-", "");
+    window.location.href = "/book/detail/"+id+"/"+string_to_slug(title);
+  });
+}
+
+function string_to_slug (str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to   = "aaaaeeeeiiiioooouuuunc------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+}
