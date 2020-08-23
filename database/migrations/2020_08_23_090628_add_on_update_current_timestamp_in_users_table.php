efbdf50e5d7f600b32f04540bbde61891276c6d2
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-08-23 09:15:02 +0700

    Migration : AddOnUpdateCurrentTimestampInUsersTable

---
+++ database/migrations/2020_08_23_090628_add_on_update_current_timestamp_in_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOnUpdateCurrentTimestampInUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE `users` CHANGE `updated_at` `updated_at` TIMESTAMP on update CURRENT_TIMESTAMP NULL DEFAULT NULL');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('ALTER TABLE `users` CHANGE `updated_at` `updated_at` TIMESTAMP NULL DEFAULT NULL');
+    }
+}
