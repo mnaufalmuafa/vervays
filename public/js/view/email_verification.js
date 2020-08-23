eef3315b5cde7e78757967e2fba4821328e7ee7b
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-08-24 06:34:13 +0700

    Memperbaiki alert pada halaman email verifivation

--- public/js/view/email_verification.js
+++ public/js/view/email_verification.js
@@ -3,15 +3,22 @@ $(document).ready(function() {
 	console.log('ready');
   $('#btnLogout').click(function(event) {
 		event.preventDefault();
-		console.log('dsg');
-    var isLogout = confirm("Apakah yakin ingin logout?");
-    if (isLogout) {
-      $.ajax({
-        url : "/logout",
-        method : "POST",
-      }).done(function() {
-        window.location.href = "/";
-      });
-    }
+    Swal.fire({
+      title: 'Apakah anda yakin ingin logout?',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Ya',
+      cancelButtonText : 'Tidak'
+    }).then((result) => {
+      if (result.value) {
+        $.ajax({
+          url : "/logout",
+          method : "POST",
+        }).done(function() {
+          window.location.href = "/";
+        });
+      }
+    });
   });
 });
