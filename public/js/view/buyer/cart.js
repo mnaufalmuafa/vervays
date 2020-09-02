7e3ab21c0726fa4d55fa62d291682014e6d21523
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-09-02 17:03:38 +0700

    Mengimplementasikan vuue pada halaman cart

--- public/js/view/buyer/cart.js
+++ public/js/view/buyer/cart.js
@@ -1,123 +1,87 @@
-$(document).ready(function() {
-  // displayExceptionSection();
-  getCart();
-  setFormOnSubmitListener();
-});
-
 function displayExceptionSection() {
 	var kelas = $('.exception-container').attr("class");
 	kelas = kelas.replace("none", "block");
-	$('.exception-container').attr("class", kelas);
-}
-
-function displayMainSection() {
-  var kelas = $('.main-container').attr("class");
-	kelas = kelas.replace("none", "block");
-	$('.main-container').attr("class", kelas);
-}
-
-function getCart() {
-	$.ajax({
-    url : "/get/get_user_cart/",
-    method : "get"
-  }).done(function(books) {
-    // console.log(books);
-    if (books.length === 0) { //Jika keranjang belanja masih kosong
-      displayExceptionSection();
-      $('#btnSearchBook').click(function() {
-        window.location.href = "/";
-      });
-    }
-    else { // Jika keranjang belanja sudah terisi (minimal 1 buku)
-      displayMainSection();
-      showCart(books);
-    }
+  $('.exception-container').attr("class", kelas);
+  $('#btnSearchBook').click(function() {
+    window.location.href = "/";
   });
 }
 
-function showCart(books) {
-  var template = document.querySelector('#bookTemplate');
-  var container = document.querySelector('#bookContainer');
-  var totalPrice = 0;
-  for (let i = 0; i < books.length; i++) {
-    var clone = template.content.cloneNode(true);
-    var ebookCoverURL = "/ebook/ebook_cover/"+books[i].ebookCoverId+"/"+books[i].ebookCoverName;
-    clone.querySelector('h4').innerHTML = books[i].title;
-    clone.querySelector('p.authorInfo span').innerHTML = books[i].author;
-    clone.querySelector('p.publisherDetail').innerHTML = books[i].publisherName;
-    clone.querySelector('p.price span').innerHTML = books[i].priceForHuman;
-    clone.querySelector('div').setAttribute("data-book-id", books[i].bookId);
-    clone.querySelector('div').setAttribute("data-price", books[i].price);
-    clone.querySelector('img').setAttribute("src", ebookCoverURL);
-    clone.querySelector('img').setAttribute("data-book-id", books[i].bookId);
-    clone.querySelector('img').setAttribute("data-book-title", books[i].title);
-    clone.querySelector('.book-title').setAttribute("data-book-id", books[i].bookId);
-    clone.querySelector('.book-title').setAttribute("data-book-title", books[i].title);
-    clone.querySelector('.ic-trash').setAttribute("data-book-id", books[i].bookId);
-    clone.querySelector('.ic-trash').setAttribute("data-book-title", books[i].title);
-    totalPrice = totalPrice + books[i].price;
-    container.appendChild(clone);
+function getPaymentMethodValue() {
+  var radios = document.getElementsByName('paymentMethod');
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      return radios[i].value;
+    }
   }
-  setBookOnClickListener();
-  setTrashIconOnClickListener();
-  $('#total-amount span').html(convertToRupiah(totalPrice));
 }
 
-function setBookOnClickListener() {
-  $(".book-title").click(function() {
-    var id = $(this).attr("data-book-id");
-    var title = $(this).attr("data-book-title");
-    window.location.href = "/book/detail/"+id+"/"+string_to_slug(title);
-  });
-  $(".book-cover").click(function() {
-    var id = $(this).attr("data-book-id");
-    var title = $(this).attr("data-book-title");
-    window.location.href = "/book/detail/"+id+"/"+string_to_slug(title);
-  });
-}
-
-function setTrashIconOnClickListener() {
-  $(".ic-trash").click(function() {
-    var id = $(this).attr("data-book-id");
-    var title = $(this).attr("data-book-title");
-    Swal.fire({
-      title: "Apakah anda yakin akan menghapus buku \""+title+"\" dari keranjang ?",
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Ya',
-      cancelButtonText : 'Tidak'
-    }).then((result) => {
-      if (result.value) {
-        $.ajax({
-          url : "/post/remove_book_from_cart/"+id,
-          method : "POST"
-        }).done(function() {
-          location.reload();
-        });
+var listBook = new Vue({
+  el : "#mainContainer",
+  data : {
+    books : null
+  },
+  beforeCreate : function() {
+    fetch("/get/get_user_cart")
+      .then(response => response.json())
+      .then(data => {
+        this.books = data;
+        console.log(this.books);
+        if (this.books.length == 0) {
+          hideMainSection();
+          displayExceptionSection();
+        }
+      })
+  },
+  filters : {
+    cardBookId : function(id) {
+      return "book-card-"+id;
+    },
+    currencyFormat : function(value) {
+      return convertToRupiah(value);
+    },
+    ebookCoverURL : function(value, ebookCoverId, ebookCoverName) {
+      return "/ebook/ebook_cover/"+ebookCoverId+"/"+ebookCoverName;
+    },
+    bookDetailURL : function(value, title) {
+      return "/book/detail/"+value+"/"+string_to_slug(title);
+    },
+  },
+  computed : {
+    totalAmount : function() {
+      var total = 0;
+      for (var el of this.books) {
+        total = total + Number(el.price);
       }
-    });
-  });
-}
-
-function setFormOnSubmitListener() {
-  $('#orderForm').submit(function(e) {
-    e.preventDefault();
-    $(':input[type="submit"]').prop('disabled', true);
-    $(':input[type="submit"]').html('....');
-    var bookCount = 0;
-    $(".book-item").each(function() {
-      bookCount++;
-    });
-    if (bookCount > 0) {
-      var arrBookId = [];
-      $(".book-item").each(function() {
-        arrBookId.push($(this).attr("data-book-id"));
+      return total;
+    }
+  },
+  methods : {
+    deleteBook : function(id, title, index) {
+      Swal.fire({
+        title: "Apakah anda yakin ingin menghapus buku \""+title+"\" dari keranjang belanja?",
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Ya',
+        cancelButtonText : 'Tidak'
+      }).then((result) => {
+        if (result.value) {
+          $.ajax({
+            url : "/post/remove_book_from_cart/"+id,
+            method : "POST"
+          });
+          this.books.splice(index, 1);
+          showAlert("Buku \""+title+"\" telah dihapus dari keranjang belanja", "success");
+          if (this.books.length == 0) {
+            displayExceptionSection();
+          }
+        }
       });
+    },
+    submit : function() {
       paymentMethod = getPaymentMethodValue();
-      var bookData = {arrBookId, paymentMethod};
-      console.log(bookData);
-
+      console.log({ paymentMethod });
       $.ajax({
         url : "http://127.0.0.1:8000/post/create_order",
         method : "POST",
@@ -132,14 +96,5 @@ function setFormOnSubmitListener() {
         window.location.href = "/info/order/"+orderId;
       });
     }
-  });
-}
-
-function getPaymentMethodValue() {
-  var radios = document.getElementsByName('paymentMethod');
-  for (var i = 0, length = radios.length; i < length; i++) {
-    if (radios[i].checked) {
-      return radios[i].value;
-    }
   }
-}
+});
--- resources/views/pages/buyer/cart.blade.php
+++ resources/views/pages/buyer/cart.blade.php
@@ -18,53 +18,68 @@
       <button class="btn btn-danger" id="btnSearchBook">Cari buku</button>
     </div>
   </div>
-  <div class="container-fluid main-container d-none">
+  <div class="container-fluid main-container" id="mainContainer" v-if="books.length > 0">
     <div class="row">
       <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-xs-12" id="bookContainer">
-        <template id = "bookTemplate">
-          <div class="book-item mt-2" data-book-id="" data-price="">
-            <div class="row">
-              <div class="col-2">
-                <img 
-                  src="{{ url('image/book_placeholder.png') }}" 
-                  alt=""
-                  class="book-cover"
-                  data-book-id="">
-              </div>
-              <div class="col-10">
-                <h4 class="font-weight-bold book-title" data-book-id="">Judul Buku</h4>
-                <p class="authorInfo mb-0">Ditulis oleh <span>Nama penulis</span></p>
-                <p class="publisherDetail mt-1 mb-0">Nama penerbit</p>
-                <div class="row justify-content-between mt-4">
-                  <div class="col-4">
-                    <p class="price d-inline">Rp. <span>34.000</span></p>
-                  </div>
-                  <div class="col-4">
+        {{-- Book Container with Vue --}}
+        <transition-group
+          name="fade" 
+          enter-active-class="animated fadeIn" 
+          leave-active-class="animated fadeOutLeft">
+          <div 
+            v-for="(book, index) in books"
+            :key="book.bookId"
+            :id="book.bookId | cardBookId">
+            <div class="book-item mt-2">
+              <div class="row">
+                <div class="col-2">
+                  <a 
+                    :href="book.bookId | bookDetailURL(book.title)">
                     <img 
-                      src="{{ url('/image/ic_trash.png') }}" 
-                      alt="Trash Icon"
-                      class="ic-trash d-inline mr-3"
-                      data-book-id="">
+                      :src="book.id | ebookCoverURL(book.ebookCoverId, book.ebookCoverName)" 
+                      alt=""
+                      class="book-cover">
+                  </a>
+                </div>
+                <div class="col-10">
+                  <a 
+                    :href="book.bookId | bookDetailURL(book.title)">
+                    <h4 class="font-weight-bold book-title" data-book-id="">@{{ book.title }}</h4>
+                  </a>
+                  <p class="authorInfo mb-0">Ditulis oleh <span>@{{ book.author }}</span></p>
+                  <p class="publisherDetail mt-1 mb-0">@{{ book.publisherName }}</p>
+                  <div class="row justify-content-between mt-4">
+                    <div class="col-4">
+                      <p class="price d-inline">@{{ book.price | currencyFormat }}</p>
+                    </div>
+                    <div class="col-4">
+                      <img 
+                        src="{{ url('/image/ic_trash.png') }}" 
+                        alt="Trash Icon"
+                        class="ic-trash d-inline mr-3"
+                        @click="deleteBook(book.bookId, book.title, index)">
+                    </div>
                   </div>
                 </div>
               </div>
-            </div> {{-- end row --}}
-            <hr>
+              <hr>
+            </div>
           </div>
-        </template>
+        </transition-group>
       </div>
-      <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
+      {{-- Sidebar --}}
+      <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12" id="paymentSidebar">
         <div class="card px-2 py-2">
           <div class="card-first-section row pr-3">
             <p class="d-inline col">Total harga</p>
-            <p class="d-inline text-right col" id="total-amount"><span>156.000</span></p>
+            <p class="d-inline text-right col" id="total-amount"><span>@{{ totalAmount | currencyFormat }}</span></p>
           </div>
           <hr>
           <div class="card-second-section">
             <p>Pilih metode pembayaran</p>
             <form 
               id="orderForm"
-              action=""
+              v-on:submit.prevent="submit()"
               class="pl-2 pb-2">
               <input type="radio" name="paymentMethod" id="BNIOption" value="1" required>
               <label for="BNIOption">BNI Virtual Account (Bisa ditansfer dari semua bank)</label><br>
