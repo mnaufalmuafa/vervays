c32937a701c1b122ad94daab5bd8f87ccba32ed8
mnaufalmuafa <naufalmuafa77@gmail.com>
2021-02-13 14:31:46 +0700

    Meresponsifkan halaman account_setting

--- public/js/view/buyer/account_setting.js
+++ public/js/view/buyer/account_setting.js
@@ -1,11 +1,182 @@
 $(document).ready(function(){
-  resetMenu();
-	setUpGenderRadioButton();
-	setChangeProfileFormOnSubmitListener();
-	setUpChangePasswordForm();
-	setUpDeleteAccount();
-	setupSidebar();
 	setUpAlert();
+});
+
+var accountSetting = new Vue({
+  el : "#content",
+  data : {
+    selectedSection : 1,
+		userGender : $('meta[name=userGender]').attr("content"),
+  },
+  beforeCreate : function() {
+    resetMenu();
+		choiceMenu("MenuUbahProfil", "section-ubah-profil");
+		setupMonthJoined();
+  },
+	created : function created() {
+		this.hideCollapsedWindow();
+		var width = $(window).width();
+		if (width < 900) {
+			$("#sidebar").hide();
+			this.showRightArrow();
+		}
+		else {
+			this.hideRightArrow();
+		}
+		window.addEventListener("resize", this.windowChangeListener);
+	},
+	methods : {
+		changeSelectedSection : function(ss, menuId, sectionId) {
+			resetMenu();
+			this.selectedSection = Number(ss);
+			choiceMenu(menuId, sectionId);
+      this.hideCollapsedWindow();
+		},
+		windowChangeListener : function() {
+			var width = $(window).width();
+			if (width < 900) {
+				$("#sidebar").hide();
+				this.showRightArrow();
+			}
+			else {
+				$("#sidebar").show();
+				this.hideRightArrow();
+			}
+		},
+		showCollapsedWindow : function() {
+			$("#collapsedWindow").show();
+			this.hideRightArrow();
+		},
+		hideCollapsedWindow : function() {
+			$("#collapsedWindow").hide();
+			var width = $(window).width();
+			if (width < 900) {
+				this.showRightArrow();
+			}
+			else {
+				this.hideRightArrow();
+			}
+		},
+		showRightArrow : function() {
+			$("#rightArrow").show();
+		},
+		hideRightArrow : function() {
+			$("#rightArrow").hide();
+		},
+		submitChangeProfileForm : function() {
+			var firstName = $("input[name=firstName]").val();
+			var lastName = $("input[name=lastName]").val();
+			var birthDay = $("#inputBirthDay").val();
+			var phoneNum = $("#inputPhoneNumber").val();
+			var gender = "";
+			if ($("#inputRadioLK:checked").val()) {
+				gender = "male";
+			}
+			else if ($("#inputRadioPR:checked").val()) {
+				gender = "female";
+			}
+			$.ajax({
+				url : "/post/update_profile",
+				method : "POST",
+				data : {
+					firstName : firstName,
+					lastName : lastName,
+					birthDay : birthDay,
+					phoneNum : phoneNum,
+					gender : gender
+				}
+			}).done(function() {
+				storeFlashMessage("Berhasil mengubah profil", "success", 2);
+				location.reload();
+			});
+		},
+		submitChangePasswordForm : function() {
+			$("#btnSubmitChangePassword").prop('disabled', true);
+			$.ajax({
+				url : "/post/is_password_true",
+				method : "POST",
+				data : {
+					password : $("#inputOldPassword").val()
+				}
+			}).done(function(isPasswordTrue) {
+				if (!isPasswordTrue) { // Jika password lamanya tidak benar
+					$("#errorOldPassword").html("Password tidak sesuai");
+					$("#errorOldPassword").attr("class", "");
+					$("#btnSubmitChangePassword").prop('disabled', false);
+				}
+				else if ($("#inputNewPassword").val() == $("#inputRetypeNewPassword").val() && $("#inputRetypeNewPassword").val().length > 7) { // jika benar
+					$.ajax({
+						url : "/post/update_password",
+						method : "POST",
+						data : {
+							password : $("#inputNewPassword").val()
+						}
+					}).done(function() {
+						storeFlashMessage("Berhasil mengubah password", "success", 2);
+						location.reload();
+					});
+				}
+				else {
+					$("#btnSubmitChangePassword").prop('disabled', false);
+				}
+			});
+		},
+		deleteAccount : function() {
+			Swal.fire({
+				title: 'Apakah anda yakin ingin menghapus akun ?',
+				showCancelButton: true,
+				confirmButtonColor: '#d33',
+				cancelButtonColor: '#3085d6',
+				confirmButtonText: 'Ya',
+				cancelButtonText : 'Tidak'
+			}).then((result) => {
+				if (result.value) {
+					$.ajax({
+						url : "/post/destroy_account",
+						method : "POST"
+					}).done(function(res) {
+						if (res != "success") {
+							Swal.fire({
+								icon: 'error',
+								title: 'Oops...',
+								text: res,
+							});
+						}
+						else {
+							window.location.href = "/";
+						}
+					});
+				}
+			});
+		},
+		inputNewPasswordKeyupListener : function() {
+			if ($("#inputNewPassword").val().length < 8) {
+				$("#errorNewPassword").html("Password minimal terdiri dari 8 karaker");
+				$("#errorNewPassword").attr("class", "");
+			}
+			else {
+				$("#errorNewPassword").attr("class", "d-none");
+			}
+			if ($("#inputRetypeNewPassword").val() !== "") {
+				if ($("#inputNewPassword").val() != $("#inputRetypeNewPassword").val()) {
+					$("#errorRetypeNewPassword").html("Password tidak sama");
+					$("#errorRetypeNewPassword").attr("class", "");
+				}
+				else {
+					$("#errorRetypeNewPassword").attr("class", "d-none");
+				}
+			}
+		},
+		inputRetypeNewPasswordKeyupListener : function() {
+			if ($("#inputNewPassword").val() != $("#inputRetypeNewPassword").val()) {
+				$("#errorRetypeNewPassword").html("Password tidak sama");
+				$("#errorRetypeNewPassword").attr("class", "");
+			}
+			else {
+				$("#errorRetypeNewPassword").attr("class", "d-none");
+			}
+		}
+	}
 });
 
 function resetMenu() {
@@ -16,9 +187,15 @@ function resetMenu() {
 }
 
 function choiceMenu(menuId, sectionId) {
-	$("#" + menuId + " div").attr("class", $("#" + menuId +" div").attr("class").replace("white", "red")); //setup sign
-	$("#" + menuId + " p").css("color", "red"); // mengubah teks menjadi merah
+	$("#" + menuId + " div").attr("class", $("#" + menuId +" div").attr("class").replace("white", "red"));
+  $("#" + menuId + "Sidebar div").attr("class", $("#" + menuId +" div").attr("class").replace("white", "red")); //setup sign
+
+	$("#" + menuId + " p").css("color", "red");
+  $("#" + menuId + "Sidebar p").css("color", "red"); // mengubah teks menjadi merah
+
 	$("#" + menuId + " hr").attr("class", "red-hr"); // mengubah warna hr menjadi merah
+  $("#" + menuId + "Sidebar hr").attr("class", "red-hr"); // mengubah warna hr menjadi merah
+
 	$("#"+sectionId).show();
 }
 
@@ -62,154 +239,4 @@ function setupMonthJoined() {
 			$("#memberJoinFrom span").text("Des");
 			break;
 	}
-}
-
-function setupSidebar() {
-  choiceMenu("MenuUbahProfil", "section-ubah-profil");
-	setupMonthJoined();
-	$("#MenuUbahProfil").click(function() {
-		resetMenu();
-		choiceMenu("MenuUbahProfil", "section-ubah-profil");
-	});
-	$("#MenuUbahPassword").click(function() {
-		resetMenu();
-		choiceMenu("MenuUbahPassword", "section-ubah-password");
-	});
-	$("#MenuHapusAkun").click(function() {
-		resetMenu();
-		choiceMenu("MenuHapusAkun", "section-hapus-akun");
-	});
-}
-
-function setUpGenderRadioButton() {
-	var gender = $('meta[name=userGender]').attr("content");
-	if (gender == "male") {
-		$("#inputRadioLK").prop('checked', true);
-	}
-	else if (gender == "female") {
-		$("#inputRadioPR").prop('checked', true);
-	}
-}
-
-function setChangeProfileFormOnSubmitListener() {
-	$("#changeProfileForm").on("submit", function(event) {
-		event.preventDefault();
-		var firstName = $("input[name=firstName]").val();
-		var lastName = $("input[name=lastName]").val();
-		var birthDay = $("#inputBirthDay").val();
-		var phoneNum = $("#inputPhoneNumber").val();
-		var gender = "";
-		if ($("#inputRadioLK:checked").val()) {
-			gender = "male";
-		}
-		else if ($("#inputRadioPR:checked").val()) {
-			gender = "female";
-		}
-		$.ajax({
-			url : "/post/update_profile",
-			method : "POST",
-			data : {
-				firstName : firstName,
-				lastName : lastName,
-				birthDay : birthDay,
-				phoneNum : phoneNum,
-				gender : gender
-			}
-		}).done(function() {
-			storeFlashMessage("Berhasil mengubah profil", "success", 2);
-			location.reload();
-		});
-	});
-}
-
-function setUpChangePasswordForm() {
-	setUpInputNewPassword();
-	setUpInputRetypeNewPassword();
-	$("#changePasswordForm").on("submit", function(event) {
-		event.preventDefault();
-		$("#btnSubmitChangePassword").prop('disabled', true);
-		$.ajax({
-			url : "/post/is_password_true",
-			method : "POST",
-			data : {
-				password : $("#inputOldPassword").val()
-			}
-		}).done(function(isPasswordTrue) {
-			if (!isPasswordTrue) { // Jika password lamanya tidak benar
-				$("#errorOldPassword").html("Password tidak sesuai");
-				$("#errorOldPassword").attr("class", "");
-				$("#btnSubmitChangePassword").prop('disabled', false);
-			}
-			else if ($("#inputNewPassword").val() == $("#inputRetypeNewPassword").val() && $("#inputRetypeNewPassword").val().length > 7) { // jika benar
-				$.ajax({
-					url : "/post/update_password",
-					method : "POST",
-					data : {
-						password : $("#inputNewPassword").val()
-					}
-				}).done(function() {
-					storeFlashMessage("Berhasil mengubah password", "success", 2);
-					location.reload();
-				});
-			}
-			else {
-				$("#btnSubmitChangePassword").prop('disabled', false);
-			}
-		});
-	});
-}
-
-function setUpInputNewPassword() {
-	$("#inputNewPassword").keyup(function() {
-		if ($("#inputNewPassword").val().length < 8) {
-			$("#errorNewPassword").html("Password minimal terdiri dari 8 karaker");
-			$("#errorNewPassword").attr("class", "");
-		}
-		else {
-			$("#errorNewPassword").attr("class", "d-none");
-		}
-	});
-}
-
-function setUpInputRetypeNewPassword() {
-	$("#inputRetypeNewPassword").keyup(function() {
-		if ($("#inputNewPassword").val() != $("#inputRetypeNewPassword").val()) {
-			$("#errorRetypeNewPassword").html("Password tidak sama");
-			$("#errorRetypeNewPassword").attr("class", "");
-		}
-		else {
-			$("#errorRetypeNewPassword").attr("class", "d-none");
-		}
-	});
-}
-
-function setUpDeleteAccount() {
-	$("#deleteAccount").click(function() {
-    Swal.fire({
-      title: 'Apakah anda yakin ingin menghapus akun ?',
-      showCancelButton: true,
-      confirmButtonColor: '#d33',
-      cancelButtonColor: '#3085d6',
-      confirmButtonText: 'Ya',
-      cancelButtonText : 'Tidak'
-    }).then((result) => {
-      if (result.value) {
-        $.ajax({
-          url : "/post/destroy_account",
-          method : "POST"
-        }).done(function(res) {
-          if (res != "success") {
-            Swal.fire({
-              icon: 'error',
-              title: 'Oops...',
-              text: res,
-            });
-          }
-          else {
-            window.location.href = "/";
-          }
-        });
-      }
-    });
-	});
 }
--- resources/views/pages/buyer/account_setting.blade.php
+++ resources/views/pages/buyer/account_setting.blade.php
@@ -17,140 +17,252 @@
 @endsection
 
 @section('content')
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-2">
-				<div class="d-flex justify-content-center">
-					<p id="userName">{{ $user->name }}</p>
-				</div>
-				<div class="d-flex justify-content-center">
-					<p id="memberJoinFrom">Member sejak <span>{{ $user->created_at_month }}</span> {{ $user->created_at_year }}</p>
-				</div>
+  <div id="content">
+    <div id="collapsedWindow">
+      <div id="collapsedSidebar">
+        <div 
+          class="menu-sidebar" 
+          id="MenuUbahProfilSidebar"
+          @click="changeSelectedSection(1, 'MenuUbahProfil', 'section-ubah-profil')">
+          <div class="menu-sign d-inline-block menu-sign-red"></div>
+          <p class="d-inline-block">Ubah Profil</p>
+          <hr class="red-hr">
+        </div>
+        <div 
+          class="menu-sidebar" 
+          id="MenuUbahPasswordSidebar"
+          @click="changeSelectedSection(2, 'MenuUbahPassword', 'section-ubah-password')">
+          <div class="menu-sign d-inline-block menu-sign-white"></div>
+          <p class="d-inline-block">Ubah Password</p>
+          <hr>
+        </div>
+        <div 
+          class="menu-sidebar" 
+          id="MenuHapusAkunSidebar"
+          @click="changeSelectedSection(3, 'MenuHapusAkun', 'section-hapus-akun')">
+          <div class="menu-sign d-inline-block menu-sign-white"></div>
+          <p class="d-inline-block">Hapus Akun</p>
+          <hr>
+        </div>
+      </div>
+      <img 
+        id="closeCollapsedWindow"
+        src="{{ url("/image/close_button.png") }}"
+        @click="hideCollapsedWindow()">
+    </div>
+    <img 
+      id="rightArrow"
+      src="{{ url("/image/right_arrow.png") }}"
+      @click="showCollapsedWindow()">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-2" id="sidebar">
+          <div class="d-flex justify-content-center">
+            <p id="userName">{{ $user->name }}</p>
+          </div>
+          <div class="d-flex justify-content-center">
+            <p id="memberJoinFrom">Member sejak <span>{{ $user->created_at_month }}</span> {{ $user->created_at_year }}</p>
+          </div>
 
-				<div class="menu-sidebar" id="MenuUbahProfil">
-					<div class="menu-sign d-inline-block menu-sign-red"></div>
-					<p class="d-inline-block">Ubah Profil</p>
-					<hr class="red-hr">
-				</div>
-				<div class="menu-sidebar" id="MenuUbahPassword">
-					<div class="menu-sign d-inline-block menu-sign-white"></div>
-					<p class="d-inline-block">Ubah Password</p>
-					<hr>
-				</div>
-				<div class="menu-sidebar" id="MenuHapusAkun">
-					<div class="menu-sign d-inline-block menu-sign-white"></div>
-					<p class="d-inline-block">Hapus Akun</p>
-					<hr>
-				</div>
-			</div>
-			<div class="col-10">
-				<section class="section-content" id="section-ubah-profil">
-					<form id="changeProfileForm" class="mb-2">
-						<h3 class="font-weight-bold">Ubah Profil</h3>
-						<div class="form-row">
-							<div class="form-group col-md-6">
-								<label for="inputFirstName">Nama depan</label>
-								<input 
-									type="text" 
-									name="firstName" 
-									id="inputFirstName" 
-									class="form-control" 
-									required 
-									value="{{ $user->firstName }}">
-								<small id="errorFirstName" class="d-none">error nama depan</small>
-							</div>
-							<div class="form-group col-md-6">
-								<label for="inputLastName">Nama belakang</label>
-								<input 
-									type="text" 
-									name="lastName" 
-									id="inputLastName"
-									value="{{ $user->lastName }}"
-									class="form-control">
-								<small id="errorLastName" class="d-none">error nama belakang</small>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="inputBirthDay">Tanggal Lahir (opsional)</label>
-							<input 
-								type="date" 
-								class="form-control" 
-								max="{{ $currentDate }}" 
-								id="inputBirthDay" 
-								name="birthDay"
-								value="{{ $user->birthDay }}">
-							<small id="errorBirthDay" class="d-none">error tanggal lahir</small>
-						</div>
-						<div class="form-group">
-							<label for="inputPhoneNumber">No HP (opsional)</label>
-							<input 
-								type="tel" 
-								class="form-control" 
-								id="inputPhoneNumber" 
-								name="phoneNumber"
-								maxlength="12"
-								minlength="10"
-								pattern="[0]{1}[8]{1}[0-9]{8,10}"
-								value="{{ $user->phoneNumber ?? "" }}"
-								placeholder="08xxxxxxxxxx">
-							<small id="errorPhoneNumber" class="d-none">error NoHP</small>
-						</div>
-						<label for="">Jenis Kelamin (opsional)</label> <br>
-						<div class="form-check form-check-inline">
-							<input class="form-check-input" type="radio" name="gender" id="inputRadioLK" value="male">
-							<label class="form-check-label" for="inputRadioLK">Laki-laki</label>
-						</div>
-						<div class="form-check form-check-inline">
-							<input class="form-check-input" type="radio" name="gender" id="inputRadioPR" value="female">
-							<label class="form-check-label" for="inputRadioPR">Perempuan</label>
-						</div>
-						<div class="form-check form-check-inline">
-							<small id="errorGender" class="d-none">error Jenis Kelamin</small>
-						</div>
-						<div class="btn-container d-flex flex-row-reverse">
-							<button type="submit" class="btn btn-danger">Ubah Profil</button>
-						</div>
-					</form>
-				</section>
-				<section class="section-content" id="section-ubah-password">
-					<form id="changePasswordForm">
-						<h3 class="font-weight-bold">Ubah Password</h3>
-						<div class="form-group row">
-							<label for="inputOldPassword" class="col-sm-2 col-form-label">Password Lama</label>
-							<div class="col-sm-10">
-								<input type="password" class="form-control" id="inputOldPassword" minlength="8" required>
-								<small id="errorOldPassword" class="d-none">error Old Password</small>
-							</div>
-						</div>
-						<div class="form-group row">
-							<label for="inputNewPassword" class="col-sm-2 col-form-label">Password Baru</label>
-							<div class="col-sm-10">
-								<input type="password" class="form-control" id="inputNewPassword" minlength="8" required>
-								<small id="errorNewPassword" class="d-none">error New Password</small>
-							</div>
-						</div>
-						<div class="form-group row">
-							<label for="inputRetypeNewPassword" class="col-sm-2 col-form-label">Ulangi Password Baru</label>
-							<div class="col-sm-10">
-								<input type="password" class="form-control" id="inputRetypeNewPassword" minlength="8" required>
-								<small id="errorRetypeNewPassword" class="d-none">error Retype New Password</small>
-							</div>
-						</div>
-						<div class="btn-container d-flex flex-row-reverse">
-							<button type="submit" class="btn btn-danger" id="btnSubmitChangePassword">Ubah Password</button>
-						</div>
-					</form>
-				</section>
-				<section class="section-content" id="section-hapus-akun">
-					<h3 class="font-weight-bold">Hapus Akun</h3>
-					<div class="d-flex justify-content-between">
-						<p>Ingin menghapus akun Vervays mu?</p>
-						<p id="deleteAccount">Hapus Akun</p>
-					</div>
-				</section>
-			</div>
-		</div>
-	</div>
+          <div 
+            class="menu-sidebar" 
+            id="MenuUbahProfil"
+            @click="changeSelectedSection(1, 'MenuUbahProfil', 'section-ubah-profil')">
+            <div class="menu-sign d-inline-block menu-sign-red"></div>
+            <p class="d-inline-block">Ubah Profil</p>
+            <hr class="red-hr">
+          </div>
+          <div 
+            class="menu-sidebar" 
+            id="MenuUbahPassword"
+            @click="changeSelectedSection(2, 'MenuUbahPassword', 'section-ubah-password')">
+            <div class="menu-sign d-inline-block menu-sign-white"></div>
+            <p class="d-inline-block">Ubah Password</p>
+            <hr>
+          </div>
+          <div 
+            class="menu-sidebar" 
+            id="MenuHapusAkun"
+            @click="changeSelectedSection(3, 'MenuHapusAkun', 'section-hapus-akun')">
+            <div class="menu-sign d-inline-block menu-sign-white"></div>
+            <p class="d-inline-block">Hapus Akun</p>
+            <hr>
+          </div>
+        </div>
+        <div class="col-10" id="mainbar">
+          <section 
+            class="section-content" 
+            id="section-ubah-profil"
+            v-show="selectedSection === 1">
+            <form 
+              id="changeProfileForm" 
+              class="mb-2"
+              v-on:submit.prevent="submitChangeProfileForm()">
+              <h3 class="font-weight-bold">Ubah Profil</h3>
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="inputFirstName">Nama depan</label>
+                  <input 
+                    type="text" 
+                    name="firstName" 
+                    id="inputFirstName" 
+                    class="form-control" 
+                    required 
+                    value="{{ $user->firstName }}">
+                  <small id="errorFirstName" class="d-none">error nama depan</small>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="inputLastName">Nama belakang</label>
+                  <input 
+                    type="text" 
+                    name="lastName" 
+                    id="inputLastName"
+                    value="{{ $user->lastName }}"
+                    class="form-control">
+                  <small id="errorLastName" class="d-none">error nama belakang</small>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputBirthDay">Tanggal Lahir (opsional)</label>
+                <input 
+                  type="date" 
+                  class="form-control" 
+                  max="{{ $currentDate }}" 
+                  id="inputBirthDay" 
+                  name="birthDay"
+                  value="{{ $user->birthDay }}">
+                <small id="errorBirthDay" class="d-none">error tanggal lahir</small>
+              </div>
+              <div class="form-group">
+                <label for="inputPhoneNumber">No HP (opsional)</label>
+                <input 
+                  type="tel" 
+                  class="form-control" 
+                  id="inputPhoneNumber" 
+                  name="phoneNumber"
+                  maxlength="12"
+                  minlength="10"
+                  pattern="[0]{1}[8]{1}[0-9]{8,10}"
+                  value="{{ $user->phoneNumber ?? "" }}"
+                  placeholder="08xxxxxxxxxx">
+                <small id="errorPhoneNumber" class="d-none">error NoHP</small>
+              </div>
+              <label for="">Jenis Kelamin (opsional)</label> <br>
+              <div class="form-check form-check-inline">
+                <input 
+                  class="form-check-input" 
+                  type="radio" 
+                  name="gender" 
+                  id="inputRadioLK" 
+                  value="male" 
+                  :checked="userGender == 'male'">
+                <label 
+                  class="form-check-label" 
+                  for="inputRadioLK">Laki-laki</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input 
+                  class="form-check-input" 
+                  type="radio" 
+                  name="gender" 
+                  id="inputRadioPR" 
+                  value="female" 
+                  :checked="userGender == 'female'">
+                <label 
+                  class="form-check-label" 
+                  for="inputRadioPR">Perempuan</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <small id="errorGender" class="d-none">error Jenis Kelamin</small>
+              </div>
+              <div class="btn-container d-flex flex-row-reverse">
+                <button type="submit" class="btn btn-danger">Ubah Profil</button>
+              </div>
+            </form>
+          </section>
+          <section 
+            class="section-content" 
+            id="section-ubah-password"
+            v-show="selectedSection === 2">
+            <form 
+              id="changePasswordForm"
+              v-on:submit.prevent="submitChangePasswordForm()">
+              <h3 class="font-weight-bold">Ubah Password</h3>
+              <div class="form-group row">
+                <label 
+                  for="inputOldPassword" 
+                  class="col-sm-2 col-form-label">Password Lama</label>
+                <div class="col-sm-12">
+                  <input 
+                    type="password" 
+                    class="form-control" 
+                    id="inputOldPassword" 
+                    minlength="8" 
+                    required>
+                  <small 
+                    id="errorOldPassword" 
+                    class="d-none">error Old Password</small>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label 
+                  for="inputNewPassword" 
+                  class="col-sm-12 col-form-label">Password Baru</label>
+                <div class="col-sm-12">
+                  <input 
+                    type="password" 
+                    class="form-control" 
+                    id="inputNewPassword" 
+                    minlength="8" 
+                    v-on:keyup="inputNewPasswordKeyupListener" 
+                    required>
+                  <small 
+                    id="errorNewPassword" 
+                    class="d-none">error New Password</small>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label 
+                  for="inputRetypeNewPassword" 
+                  class="col-sm-12 col-form-label">Ulangi Password Baru</label>
+                <div class="col-sm-12">
+                  <input 
+                    type="password" 
+                    class="form-control" 
+                    id="inputRetypeNewPassword" 
+                    minlength="8" 
+                    v-on:keyup="inputRetypeNewPasswordKeyupListener"
+                    required>
+                  <small 
+                    id="errorRetypeNewPassword" 
+                    class="d-none">error Retype New Password</small>
+                </div>
+              </div>
+              <div 
+                class="btn-container d-flex flex-row-reverse">
+                <button 
+                  type="submit" 
+                  class="btn btn-danger" 
+                  id="btnSubmitChangePassword">Ubah Password</button>
+              </div>
+            </form>
+          </section>
+          <section 
+            class="section-content" 
+            id="section-hapus-akun"
+            v-show="selectedSection === 3">
+            <h3 class="font-weight-bold">Hapus Akun</h3>
+            <div class="d-flex justify-content-between">
+              <p>Ingin menghapus akun Vervays mu?</p>
+              <p 
+                id="deleteAccount"
+                @click="deleteAccount()">Hapus Akun</p>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
 
 @push('add-on-script')
